09b109f9d8edf5819396a7c6fb80ef70d3291116
Ilma <ilmaprova13@gmail.com>
2022-05-10 15:11:44 +0600

    tailwind init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Home from './Pages/Home/Home/home'
+import Login from './Pages/Authentication/Login/Login'
+import Registration from './Pages/Authentication/Registration/Registration'
+import Header from './Pages/Shared/Header/header'
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Registration></Registration>}></Route>
+      </Routes>
     </div>
   );
 }
